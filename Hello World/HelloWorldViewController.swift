3d3f6f3d16cd996be56d48be16ca8262860a8e3a
Kwabena Mensah <kamn83@mail.missouri.edu>
2019-02-08 23:38:13 -0600

    Implemented actions

---
+++ Hello World/HelloWorldViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HelloWorldViewController.swift
+//  Hello World
+//
+//  Created by Kobby Mensah on 2/8/19.
+//  Copyright © 2019 Kobby Mensah. All rights reserved.
+//
+
+import UIKit
+
+class HelloWorldViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
